49bd80f84079e6534ec8de74b5b0ff1b03b125e4
Shivam singh <sanjaysinghshivam444@gmail.com>
2026-06-28 10:46:47 +0530

    Initialize Enterprise project setup

---
+++ backend/ehip-backend/src/test/java/com/shivam/ehip/EhipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.ehip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
